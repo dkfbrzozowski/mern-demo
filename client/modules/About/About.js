8814b7cff4d7363d598bb3f2fa23a8d13fe90313
Dawid Brzozowski <d.brzozowski@wp.pl>
2018-03-28 10:25:49 +0200

    Create about module

---
+++ client/modules/About/About.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './About.css';
+
+class About extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+About.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
